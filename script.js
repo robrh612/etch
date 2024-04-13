14b7568b45531bf58cad7b896350a0ac54d78cec
robrh612 <53003761+robrh612@users.noreply.github.com>
2024-04-13 07:42:11 -0400

    created and styled grid

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-var container = document.querySelector(".padContainer");
+const container = document.querySelector(".padContainer");
+let rows = document.getElementsByClassName("row");
+let cells = document.getElementsByClassName("cell")
+let size = (16);
 
-for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      const cell = document.createElement('div');
+//rows
+// function etch(size){
+
+  // let sizing = 960 / 16;
+
+  for (r = 0; r < size; r++){
+    let row = document.createElement("div");
+    // row.style.width = "59px";
+    // row.style.height = "59px";
+    row.classList.add('row');
+    container.appendChild(row);
+  }
+
+for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < size; j++) {
+      let cell = document.createElement('div');
+      cell.style.width = "58px";
+      cell.style.height = "58px";
       cell.classList.add('cell');
-      container.appendChild(cell);
+      rows[j].appendChild(cell).className = "cell";
     }
   }
+  // }
+
