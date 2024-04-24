664fb51a74220e70c22a1311a0ac33082abfd564
robrh612 <53003761+robrh612@users.noreply.github.com>
2024-04-24 06:45:01 -0400

    Finished project
    reset game button created
    added code to check for less than 100 grid size selected ,
    added code to change colors of square

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Find 
 const container = document.querySelector(".padContainer");
 let rows = document.getElementsByClassName("row");
 let cells = document.getElementsByClassName("cell")
@@ -5,7 +6,10 @@ let cells = document.getElementsByClassName("cell")
 
 let size = 16;
 //rows
-// function etch(size){
+function etch(){
+
+  //clear the GD container so the new grid can be placed... 
+container.innerHTML ="";
 
 //sizing sets cell size by dividing grid area by number of size and subtracting 2px.
   let sizing = ((960 / size)-2);
@@ -17,19 +21,32 @@ let size = 16;
     container.appendChild(row);
   }
   // creates cells and attaches them to the rows
-for (let i = 0; i < rows.length; i++) {
+    for (let i = 0; i < rows.length; i++) {
     for (let j = 0; j < size; j++) {
-      let cell = document.createElement('div');
-      cell.style.width = sizing + "px";
-      cell.style.height = sizing + "px";
-      cell.classList.add('cell');
-      rows[j].appendChild(cell).className = "cell";
-  
+    let cell = document.createElement('div');
+    cell.addEventListener('mouseover', () => {
+      // cell.style.backgroundColor = 'gray';
+      cell.style.backgroundColor = '#' + Math.floor(Math.random()* 16777215).toString(16);
+    });
+    cell.style.width = sizing + "px";
+    cell.style.height = sizing + "px";
+    cell.classList.add('cell');
+    rows[j].appendChild(cell).className = "cell";
+    
     }
   }
   }
 
+
+  //new game button resets game field to the size selected.
   let newGame = () => {
-    let size = prompt("What size grid would you like?");
-    return(etch(size));
+    let newSize = prompt("What size grid would you like? Less than 100.");
+    if (newSize > 100) { 
+      alert("try again");
+      newGame();
+    } else { 
+    size = parseInt(newSize);
+    etch();
+    }
   }
+   etch();
